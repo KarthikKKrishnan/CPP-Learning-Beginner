aae205745d0a9900880300781bdffa897ee1eb59
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2026-03-12 11:29:00 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  int b;
+  int sum = 0;
+  
+  cout<<"Enter first number:\n";
+  cin>>a;
+
+  cout<<"Enter Second Number:\n";
+  cin>>b;
+
+  sum = a+b;
+  cout<<"sum is "<<sum;
+
+return 0 
+}
