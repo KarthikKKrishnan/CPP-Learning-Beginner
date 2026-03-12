fdf3c191a1c44e3bbeda1d349371eb58cfef51e2
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2026-03-12 11:31:25 +0530

    Create chtypevar.cpp

---
+++ chtypevar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a[2];   //a[] used character element count
+    cout <<"enter a char:\n";
+    cin>>a;
+    
+    cout<<a<<endl;
+    cout<<sizeof(a);
+    
+return 0;
+}
